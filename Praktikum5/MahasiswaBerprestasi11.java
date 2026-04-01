840971f2ccf0d6e00e73a4ff64ac64053cbd5071
melizahaiziK <melizaha1zik@gmail.com>
2026-04-01 10:26:53 +0700

    Praktikum 2

---
+++ Praktikum5/MahasiswaBerprestasi11.java
@@ -0,0 +1,30 @@
+package Praktikum5;
+public class MahasiswaBerprestasi11 {
+    Mahasiswa11 [] listMhs= new Mahasiswa11[5];
+    int idx;
+void tambah (Mahasiswa11 m){
+    if (idx<listMhs.length){
+        listMhs[idx]=m;
+        idx++;
+    }else {
+        System.out.println("data sudah penuh");
+    }
+}
+void tampil(){
+    for (Mahasiswa11 m:listMhs){
+        m.tampilInformasi();
+        System.out.println("-------------------------");
+    }
+}
+void bubbleSort(){
+    for (int i=0; i<listMhs.length-1; i++){
+        for (int j=1; j<listMhs.length-i; j++){
+            if (listMhs[j].ipk>listMhs[j-1].ipk){
+                Mahasiswa11 tmp = listMhs[j];
+                listMhs[j]=listMhs[j-1];
+                listMhs[j-1]=tmp;
+            }
+        }
+    }
+}
+}
