a3da4fbfbc36db9457a7b4a848a753ed9e513ad1
melizahaiziK <melizaha1zik@gmail.com>
2026-04-01 11:14:48 +0700

    praktikum 2 (selection sort)

--- Praktikum5/MahasiswaBerprestasi11.java
+++ Praktikum5/MahasiswaBerprestasi11.java
@@ -31,4 +31,17 @@ void bubbleSort(){
         }
     }
 }
+void selectionSort(){
+    for (int i = 0; i < listMhs.length - 1; i++){
+        int idxMin = i;
+        for (int j = i + 1; j < listMhs.length; j++){
+            if (listMhs[j].ipk < listMhs[idxMin].ipk){
+                idxMin = j;
+            }
+        }
+        Mahasiswa11 tmp = listMhs[idxMin];
+        listMhs[idxMin] = listMhs[i];
+        listMhs[i] = tmp;
+    }
+}
 }
